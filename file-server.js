49ee5160edb59a19ae99c57a593283b64b17b255
Nestor Urquiza <nestor.urquiza@gmail.com>
2015-05-21 12:12:50 -0400

    Create file-server.js

---
+++ file-server.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+//
+// To host a directory files put this file and package.json in it and run:
+//  npm install
+//  chmod +x file-server.js
+//  ./file-server.js
+//
+
+
+//require express and serve-index
+var express = require("express");
+var serveIndex = require('serve-index')
+
+//listen on port
+var port = 3080;
+
+//static files in same dir
+var docroot = "";
+
+//instantiate express
+var app = express();
+
+//serve all content starting at this directory
+app.use('/', serveIndex(__dirname + docroot, {'icons': true}))
+
+//start serving content
+app.listen(port);
+
+console.log("Server running at http://localhost:" + port);
